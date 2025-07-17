6e18b6e9dffb864cf3d0749e7f7e638fd4f31bca
modykray <modykray0@gmail.com>
2025-07-17 07:42:23 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,19 +2,18 @@ const venom = require('venom-bot');
 
 venom
   .create({
-    session: 'bot-session',
+    session: 'sessionName',  // اسم الجلسة ويتحفظ في فولدر sessions/sessionName
     multidevice: true
   })
-  .then(client => {
-    client.onMessage(message => {
-      if (message.body === 'م1') {
-        client.sendText(message.from, 'قسم الصور شغال ✅');
-      } else {
-        client.sendText(
-          message.from,
-          'اكتب م1 عشان أشغال القسم'
-        );
-      }
-    });
-  })
+  .then(client => start(client))
   .catch(err => console.log(err));
+
+function start(client) {
+  client.onMessage(message => {
+    if (message.body === 'م1') {
+      client.sendText(message.from, 'قسم الصور شغال ✅');
+    } else {
+      client.sendText(message.from, 'اكتب م1 عشان أشغل القسم');
+    }
+  });
+}
